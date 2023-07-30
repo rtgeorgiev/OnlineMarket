7affb72eda8bf390887371349fcd1e1c1879da3e
rtgeorgiev <rgeorgiev001@gmail.com>
2023-07-30 19:13:46 +0100

    Django models, admin page, items page, authentication mechanism

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from main import views
 urlpatterns = [
     path('home/', views.homepage, name='home'),
     path('items/', views.itemspage, name='items'),
-
+    path('login/', views.loginpage, name='login'),
+    path('logout/', views.logoutpage, name='logout'),
+    path('register/', views.registerpage, name='register'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from main.models import Item
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -6,16 +9,50 @@ def homepage(request):
     return render(request, template_name='main/home.html')
 
 def itemspage(request):
-    items = [
-        {
-            'name': 'Phone',
-            'price': '500'
-        },
-        {
-            'name': 'Laptop',
-            'price': '1000'
-        }
-    ]
-    return render(request, template_name='main/items.html', context = {'items': items})
+    if request.method == 'GET':
+        items = Item.objects.filter(owner=None)
+        return render(request, template_name='main/items.html', context={'items': items})
+    if request.method == 'POST':
+        purchased_item = request.POST.get('purchased-item')
+        if purchased_item:
+            purchased_item_object = Item.objects.get(name=purchased_item)
+            purchased_item_object.owner = request.user
+            purchased_item_object.save()
+            print(f'Congratulations! The product {purchased_item} has been bought by {request.user.username}')
+        return redirect('items')
+
+def loginpage(request):
+    if request.method == 'GET':
+        return render(request, template_name='main/login.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('items')
+        else:
+            return redirect('login')
+
+def registerpage(request):
+    if request.method == 'GET':
+        return render(request, template_name='main/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('register')
+
+def logoutpage(request):
+    pass
+
+
 
 
